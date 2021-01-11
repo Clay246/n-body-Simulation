15c0a78f63154896158da54619bc503c49dd67f2
Clay246 <63927010+Clay246@users.noreply.github.com>
2021-01-10 22:24:51 -0500

    Update n-body.py

--- n-body.py
+++ n-body.py
@@ -4,7 +4,7 @@ import matplotlib.animation as animation
 import mplanimations as ans
 import random
 
-G = .00001
+G = .00001 # The gravitational constant is set at this value for convenience
 dt = 1/100
 
 class Objects:
